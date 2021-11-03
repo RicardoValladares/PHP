4cb10255b4ec62322971f5d7dbab01eca895cbbc
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2021-11-03 08:09:26 -0600

    Update servidor.php

--- 1_funciones/servidor.php
+++ 1_funciones/servidor.php
@@ -2,13 +2,17 @@
 function AJAX($n1, $n2){
 	if(($n1!="")&&($n2!="")){
 		$resultado = floatval($n1) + floatval($n2);
-		echo(strval($resultado));
+		return (strval($resultado));
+	}
+	else{
+		return ("Solo Personal Autorizado");
 	}
 }
 if(isset($_REQUEST["n1"])&&isset($_REQUEST["n2"])){
 	$n1 = $_REQUEST["n1"];
 	$n2 = $_REQUEST["n2"];
-	AJAX($n1, $n2);
+	$retorno = AJAX($n1, $n2);
+	echo($retorno);
 }
 else{
 	echo("Solo Personal Autorizado");
