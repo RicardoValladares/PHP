cd980b7c4fb57c3ddf2527359571c9666b1d3d0d
RicardoValladares <r_a_v_r_@hotmail.com>
2022-11-12 21:24:26 -0600

    servicio

--- ws JSON/servicio.php
+++ ws JSON/servicio.php
@@ -1,11 +1,8 @@
 <?php
-	define('DB_USER', "root");
-	define('DB_PASSWORD', "123");
-	define('DB_DATABASE', "base");
-	define('DB_SERVER', "localhost");
-	define('DB_PORT', "3306");
-
-
+	if(getAuthorizationHeader() != 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2MTl9.fiBNaWdyYWNpb24gKEVsIFNhbHZhZG9yKSB+') {
+	    echo 'Crendenciales no validas';
+	    exit;
+	}
 	/*if (!isset($_SERVER['PHP_AUTH_USER'])) {
 	    header('WWW-Authenticate: Basic realm="WS con autenticacion basica"');
 	    header('HTTP/1.0 401 Unauthorized');
@@ -15,50 +12,34 @@
 	    echo 'Crendenciales no validas';
 	    exit;
 	}*/
-
-	if (!isset($_SERVER['Authorization'])) {
-	    header('HTTP/1.0 401 Unauthorized');
-	    echo 'No autenticado';
-	    exit;
-	} elseif($_SERVER['Authorization'] != 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2MTl9.fiBNaWdyYWNpb24gKEVsIFNhbHZhZG9yKSB+') {
-	    echo 'Crendenciales no validas';
-	    exit;
-	}
 	
-
 	$data = json_decode(file_get_contents('php://input'), true);
-
 	if(!isset($data["dui"]) ){
 		header("Content-type:application/json");
 		echo("{\"Estado\":\"Estructura no Valida\"}");
 		exit;
 	}
-
 	$DUI = $data["dui"];
-
-	
-	$cnx = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
-	$cnx->set_charset("utf8");
-
-	$query = "CALL valido('".$DUI."');";
-
-	$result = mysqli_query($cnx, $query);
-	if (!$result) {
-		echo mysqli_error($cnx);
-	}
-	if ($row = mysqli_fetch_array($result)) {
-
-		if($row['contador']=="0"){
-			$repuesta = array( "status"=>"error", "observacion"=>"Numero de dui agregado");
-		} else {
-			$repuesta = array( "status"=>"ok", "observacion"=>"Numero de dui repetido");
-		}
-
+	if ($DUI == '04566888-7') {
+		$repuesta = array( "status"=>"ok", "observacion"=>"Numero de dui valido");
 	} else {
-		$repuesta = array( "status"=>"error", "observacion"=>"No se logro ejecutar el proceso");
+		$repuesta = array( "status"=>"error", "observacion"=>"Numero de dui no valido");
 	}
-	
 	header("Content-type:application/json");
 	echo json_encode($repuesta);
-	
+
+
+
+function getAuthorizationHeader(){
+    $headers = null;
+    if (isset($_SERVER['Authorization'])) { $headers = trim($_SERVER["Authorization"]); }
+    else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { $headers = trim($_SERVER["HTTP_AUTHORIZATION"]); } 
+    elseif (function_exists('apache_request_headers')) { $requestHeaders = apache_request_headers(); $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders)); if (isset($requestHeaders['Authorization'])) { $headers = trim($requestHeaders['Authorization']); } }
+    return $headers;
+}
+function getBearerToken() {
+    $headers = getAuthorizationHeader();
+    if (!empty($headers)) { if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) { return $matches[1]; } }
+    return null;
+}	
 ?>
